68a043116a96fd5d56ad510b5c14d706535a65f0
ReeyaBhatia22 <reeyabhatia22@gmail.com>
2024-10-29 20:20:08 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,12 @@
+public class Function
+{
+    public static void multiply(int a, int b)
+    {
+        System.out.println("Multiplication of 2 numbers is:"+ (a*b));
+    }
+    public static void main(String[]args)
+    {
+       
+         multiply(5,6);
+    }
+}
